3ee389c16cf3aee6dee6308c66a0c38122823252
PrABpY <prabda.r@kkumail.com>
2023-06-18 10:44:26 +0700

    test lib

--- test_lib.py
+++ test_lib.py
@@ -3,7 +3,7 @@ import lib.Hyposis as lb
 Hypo = lb.Hypo()
 data = Hypo.Format("data/data4.csv")
 find_s = Hypo.FindS(data,CorrectP = "Yes",CorrectN = "No")
-# Hypo.HypoAll(data)
+Hypo.HypoAll(data)
 print(find_s)
 elimination = Hypo.Elimination(data,CorrectP = "Yes",CorrectN = ["No","-"])
 inp = ['Circular','Large','Light','Irregular','Thick']
