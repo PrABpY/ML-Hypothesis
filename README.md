9d6f1499c72500cc3dc8bbfa3259313b166b056d
PrABpY <prabda.r@kkumail.com>
2023-06-20 22:59:16 +0700

    edit

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Sample | Header1 | Header2 | ... | Result
 3 | - | - | - | (+,-) |
 4 | - | - | - | (+,-) |
 
+!! Always write the rejected hypothesis first. !!
+
 ### _Ex_
 file name "_Sample.csv_"
 Sample | Citation | Size | InLibrary | Price | Edition | Buy
@@ -26,3 +28,5 @@ Sample | Citation | Size | InLibrary | Price | Edition | Buy
 	Hypo = Hyposis.Hypo()
 	data = Hypo.Format("Sample.csv")
 	find_s = Hypo.FindS(data,CorrectP = "Yes",CorrectN = "No")
+	
+	#find_s -> [['Many', '?', 'No', '?', '?']]
--- test_lib.py
+++ test_lib.py
@@ -1,12 +1,12 @@
 import lib.Hyposis as lb
 
 Hypo = lb.Hypo()
-data = Hypo.Format("data/data4.csv")
+data = Hypo.Format("data/data3.csv")
 find_s = Hypo.FindS(data,CorrectP = "Yes",CorrectN = "No")
 Hypo.HypoAll(data)
 print(find_s)
 elimination = Hypo.Elimination(data,CorrectP = "Yes",CorrectN = ["No","-"])
-inp = ['Circular','Large','Light','Irregular','Thick']
-score = Hypo.Score(inp,product = elimination)
-print(score)
+# inp = ['Circular','Large','Light','Irregular','Thick']
+# score = Hypo.Score(inp,product = elimination)
+# print(score)
 print(elimination)
